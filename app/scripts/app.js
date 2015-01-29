a63a87e377d996f3cfd4c55e4bc536adf7181ef4
Jason Park <jason.park@laterooms.com>
2015-01-29 12:11:24 +0800

    Replace ondisplay visual effects with animation and add jshinrc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,33 @@
+/* jshint laxbreak: true */
+/* global document, window, navigator, wrap */
+
 (function(document) {
   'use strict';
 
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+
+    if (detectMobile()) {
+      document.body.classList.add('mobile');
+      window.scroll(0, 1);
+    }
   });
+
+  function detectMobile() {
+    if( navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
